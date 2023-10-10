6c8bb88f421b4bc3b2b2792906be28cff5aa3c65
Stary_Algida <piotrwc49@gmail.com>
2023-10-10 19:13:08 +0200

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "./build",
-  },
+  // build: {
+  //   outDir: "./build",
+  // },
+  base: "/CV-App/",
   plugins: [react()],
 });
